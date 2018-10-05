03d802caef3cbd98fa78a0251a665d305bcd7775
CaptainCrunch <zamoreri002@gmail.com>
2018-10-04 20:05:14 -0500

    Pit Generation

--- members/SavingPacman/runningLevel_Pits.js
+++ members/SavingPacman/runningLevel_Pits.js
@@ -2,13 +2,11 @@ class Enemy {
     constructor (x, y) {
         this.x = x;
         this.y = y;
-        this.height = 50;
-
     }
 
     show () {
         ctx.fillStyle = 'black';
-        ctx.fillRect(this.x, this.y, 150, this.height);
+        ctx.fillRect(this.x, this.y, 150, 50);
     }
 
     move () {
